6f7040f40c10eafc2ddf70007d67acfe4f2a39b5
Faisal <faisalahmed4315@gmail.com>
2024-09-28 12:11:27 +0600

    Logic Implementation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to sum of digits");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter a integer number for digit calculation: ");
+        int number = input.nextInt();
+
+
+        System.out.println(digitCalculator(number));
+
     }
+    public static int digitCalculator(int num){
+        int finalNum = 0;
+         while (num > 0){
+            int digit = num % 10;
+            finalNum += digit;
+            num/=10;
+         }
+
+        return finalNum;
+    }
+
 }
+
